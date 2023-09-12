35ea477402965d23cd1a8ee9bd2105b8cc6449ef
Andre Paim <andrepaim@gmail.com>
2023-09-12 17:10:35 -0300

    Format and comment the code

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,14 @@
 
 static const uint32_t NUM_ROWS = 15;
 
+// Constants
 const uint32_t PLANT_MAXIMUM_AGE = 10;
 const uint32_t HERBIVORE_MAXIMUM_AGE = 50;
 const uint32_t CARNIVORE_MAXIMUM_AGE = 80;
-const uint32_t MAXIMUM_ENERGY = 200;    
+const uint32_t MAXIMUM_ENERGY = 200;
 const uint32_t THRESHOLD_ENERGY_FOR_REPRODUCTION = 20;
 
+// Probabilities
 const double PLANT_REPRODUCTION_PROBABILITY = 0.2;
 const double HERBIVORE_REPRODUCTION_PROBABILITY = 0.075;
 const double CARNIVORE_REPRODUCTION_PROBABILITY = 0.025;
@@ -21,21 +23,23 @@ const double HERBIVORE_EAT_PROBABILITY = 0.9;
 const double CARNIVORE_MOVE_PROBABILITY = 0.5;
 const double CARNIVORE_EAT_PROBABILITY = 1.0;
 
-// Definitions
-
-enum entity_type_t {
+// Type definitions
+enum entity_type_t
+{
     empty,
     plant,
     herbivore,
     carnivore
 };
 
-struct pos_t {
+struct pos_t
+{
     uint32_t i;
     uint32_t j;
 };
 
-struct entity_t {
+struct entity_t
+{
     entity_type_t type;
     int32_t energy;
     int32_t age;
@@ -48,31 +52,22 @@ NLOHMANN_JSON_SERIALIZE_ENUM(entity_type_t, {
                                                 {herbivore, "H"},
                                                 {carnivore, "C"},
                                             })
-                                
+
 // Auxiliary code to convert the entity_t struct to a JSON object
-namespace nlohmann {
-    void to_json(nlohmann::json& j, const entity_t& e) {
+namespace nlohmann
+{
+    void to_json(nlohmann::json &j, const entity_t &e)
+    {
         j = nlohmann::json{{"type", e.type}, {"energy", e.energy}, {"age", e.age}};
     }
 }
 
+// Grid that contains the entities
 static std::vector<std::vector<entity_t>> entity_grid;
 
-// Function that returns the entity type grid from the entity grid
-std::vector<std::vector<entity_type_t>> get_entity_type_grid(const std::vector<std::vector<entity_t>>& entity_grid) {
-    std::vector<std::vector<entity_type_t>> entity_type_grid;
-    entity_type_grid.assign(NUM_ROWS, std::vector<entity_type_t>(NUM_ROWS, empty));
-    for (auto i = 0; i < NUM_ROWS; ++i) {
-        for (auto j = 0; j < NUM_ROWS; ++j) {
-            entity_type_grid[i][j] = entity_grid[i][j].type;
-        }
-    }
-    return entity_type_grid;
-}
-
-int main() {
+int main()
+{
     crow::SimpleApp app;
-    app.loglevel(crow::LogLevel::DEBUG);
 
     // Endpoint to serve the HTML page
     CROW_ROUTE(app, "/")
@@ -80,12 +75,11 @@ int main() {
      {
         // Return the HTML content here
         res.set_static_file_info_unsafe("../public/index.html");
-        res.end();
-    });
+        res.end(); });
 
     CROW_ROUTE(app, "/start-simulation")
         .methods("POST"_method)([](crow::request &req, crow::response &res)
-    {
+                                {
 
         std::uniform_int_distribution<> distrib(0,NUM_ROWS);
         
@@ -108,13 +102,12 @@ int main() {
 
         nlohmann::json json_grid = entity_grid; 
         res.body = json_grid.dump();
-        res.end(); 
-    });
+        res.end(); });
 
     // Endpoint to process HTTP GET requests for the next simulation iteration
     CROW_ROUTE(app, "/next-iteration")
         .methods("GET"_method)([]()
-    {
+                               {
         // Simulate the next iteration
         // Iterate over the entity grid and simulate the behaviour of each entity
         
@@ -122,8 +115,7 @@ int main() {
         
         // Return the JSON representation of the entity grid
         nlohmann::json json_grid = entity_grid; 
-        return json_grid.dump();
-    });
+        return json_grid.dump(); });
     app.port(8080).run();
 
     return 0;
